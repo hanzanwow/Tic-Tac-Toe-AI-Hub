715d0735270dc96a171b219a1503a1cd2130606c
Haszan wow <natoodxd@gmail.com>
2026-02-01 20:18:34 +0700

    create class in XO

--- TicTacToe.h
+++ TicTacToe.h
@@ -3,7 +3,31 @@
 
 #include "XOBot.h"
 
-class TicTacToe {
+class TicTacToe
+{
+private:
+    char spaces[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+    char player = 'X';
+    char computer = 'O';
+    bool running = true;
+    int PlayerWins = 0;
+    int ComputerWins = 0;
+    int Ties = 0;
+    XOBot bot;
+
+    void drawBoard();
+    void resetBoard();
+    void playerMove();
+    void computerMove();
+    char checkWinner();
+    bool checkTie();
+    bool PlayAgain();
+    void updateScore();
+    void displayScore();
+
+public:
+    TicTacToe();
+    void runGame();
 };
 
 #endif
