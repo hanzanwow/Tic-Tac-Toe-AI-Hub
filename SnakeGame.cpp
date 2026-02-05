f0328f06311e2ec15ffb59951278190ec3476f83
Haszan wow <natoodxd@gmail.com>
2026-02-06 01:54:13 +0700

    finaly but not yet

--- SnakeGame.cpp
+++ SnakeGame.cpp
@@ -4,6 +4,7 @@
 #include "SnakeGame.h"
 #include <cstdlib>
 #include "Utils.h"
+
 Snake::Snake(int start_x, int start_y){
     body.push_front({start_x, start_y});
     direction = stop;
@@ -17,16 +18,19 @@ void Snake::move(bool eaten){
     if (direction == down) new_head.y++;
     
     body.push_front(new_head);
-        if (!eaten){
-            body.pop_back();
-        }
+    if (!eaten){
+        body.pop_back();
+    }
 }
+
 bool Snake::checkSelfCollision(){
+    // เริ่มเช็คจาก i=1 เพราะ i=0 คือหัวงู (หัวชนหัวไม่ได้)
     for (size_t i = 1; i < body.size(); i++){
         if (body.front().x == body[i].x && body.front().y == body[i].y){
-            return true;
+            return true; // ชน!
         }
-        return false;
-        }
+        // ลบ return false; ตรงนี้ออก เพราะต้องรอเช็คให้ครบทุกท่อนก่อน
     }
-
+    return false; // ถ้าวนลูปครบแล้วไม่เจออะไรเลย ค่อยบอกว่าไม่ชน
+} 
+// ลบปีกกาที่เกินมาออก
